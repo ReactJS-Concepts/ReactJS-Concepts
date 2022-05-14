1b56af8fb1c06d6552c4ae5627bfe36690e7cce6
Ashik Rai <ashik.rai@lokibots.com>
2022-05-14 19:38:23 +0530

    Project Structure

---
+++ reactjs-concepts/src/App.js
@@ -0,0 +1,14 @@
+import logo from './Resources/Images/LearnReact.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs-concepts/src/ReadMe.txt
@@ -0,0 +1,5 @@
+Author:  Kihsa Iar
+Created: 14-05-2022
+
+
+This file has the link of the Medium Post that explains the concept of the code implemented in each respective branches
