655a2bc5e5c42683e50195a4a5562cbb2d4cf4ef
Ashik Rai <ashik.rai@lokibots.com>
2022-05-16 20:21:17 +0530

    Initial Folder Structure and Required Files

--- reactjs-concepts/src/App.js
+++ reactjs-concepts/src/App.js
@@ -1,11 +1,16 @@
 import logo from './Resources/Images/LearnReact.gif';
 import './App.css';
 
+import AppContainer from "./Code/AppContainer";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <img style={{width: '50%', height: 'auto'}} src={logo} alt="logo" />
+        <div>
+          <AppContainer />
+        </div>
       </header>
     </div>
   );
